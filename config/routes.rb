1358181b92d94d03977a156ffdf9c378848bd352
Kishino Yuki <yktennis1124@gmail.com>
2024-07-27 02:22:59 +0000

    [finish] 認証機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    session: 'admin/sessions'
+  }
   devise_for :users
   root to: 'homes#top'
   get 'homes/about', to: 'homes#about', as: :about

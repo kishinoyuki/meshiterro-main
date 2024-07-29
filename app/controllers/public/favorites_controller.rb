ffd0c320640dccd1a2e065379d2707afdaff5c08
Kishino Yuki <yktennis1124@gmail.com>
2024-07-29 05:40:46 +0000

    [finish] 管理者機能の実装

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,5 +1,4 @@
 class Public::FavoritesController < ApplicationController
- before_action :authenticate_user!, except: [:top]
 
   def create
     post_image = PostImage.find(params[:post_image_id])
--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
   
   scope module: :public do
    devise_for :users
-  end
-  root to: 'homes#top'
-  get 'homes/about', to: 'homes#about', as: :about
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+   root to: 'homes#top'
+   get 'homes/about', to: 'homes#about', as: :about
+   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+     resource :favorite, only: [:create, :destroy]
+     resources :post_comments, only: [:create, :destroy]
+   end
   end
   resources :users, only: [:show, :edit, :update]
 end
